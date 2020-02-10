6020070acef95690a86dbd5a55292971dcb75b42
mkbeh <mkbehforever@gmail.com>
2020-02-10 08:21:45 -0900

    Test commit

--- install.sh
+++ install.sh
@@ -1,3 +1,20 @@
+# This file is part of Fereda.
+
+# Fereda is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License 3 as published by
+# the Free Software Foundation.
+
+# Fereda is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Fereda.  If not, see <https://www.gnu.org/licenses/>.
+
+
+# Copyright (c) 2020 January mkbeh
+
 
 ## yellow            \e[1;33m \e[0m
 ## green             \e[1;32m \e[0m
