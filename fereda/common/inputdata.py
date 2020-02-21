cfe99e52145bb047111f2bbdeb1d1d1aaec9bb88
mkbeh <mkbehforever@gmail.com>
2020-02-21 09:55:32 -0900

    Fixed potential error , associated with entering an invalid regular expression.

--- fereda/common/inputdata.py
+++ fereda/common/inputdata.py
@@ -54,9 +54,17 @@ class FilesPathInputData(GenericInputData):
 
     @classmethod
     def _get_compiled_regex(cls, regexpressions: list) -> list:
-        return [
-            re.compile(rf'{regex}') for regex in regexpressions
-        ]
+        regexprs = []
+
+        for regex in regexpressions:
+            try:
+                r = re.compile(rf'{regex}')
+            except re.error:
+                raise Exception(f'Incorrect regex "{regex}". Input correct regex.')
+            else:
+                regexprs.append(r)
+
+        return regexprs
 
     @classmethod
     def generate_inputs(cls, config: dict):
