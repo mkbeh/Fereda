23c5ad2ee0f8edb8076b426b46882187f7005c01
mkbeh <mkbehforever@gmail.com>
2020-02-26 05:16:50 -0900

    Refactored

--- fereda/plugins/__init__.py
+++ fereda/plugins/__init__.py
@@ -3,22 +3,3 @@ from fereda.plugins.searchfiles import SearchFiles
 from fereda.plugins.searchappsremovedimages import SearchRemovedImages
 from fereda.plugins.searchappshiddenimages import SearchHiddenImages
 from fereda.plugins.textfileanalysis import TextFileAnalysis
-
-
-
-
-# DEFAULT_APPLICATIONS = {
-#     'Gallery': {
-#         'Gallery': 'org.gallery',
-#     },
-#
-#     'Telegram': {
-#         'Telegram (System)': 'org.telegram',
-#         'Telegram (User)': 'Telegram',
-#     },
-#
-#     'VK': {
-#         'VK (System)': 'vk.org',
-#         'VK (User)': 'VK',
-#     },
-# }
